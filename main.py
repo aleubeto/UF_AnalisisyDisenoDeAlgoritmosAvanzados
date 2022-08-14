faf8d737c0aa393071b15f4203a7ee4c45ba88d6
JuanFerrxr <jcferrere1032@gmail.com>
2022-08-13 20:29:02 -0500

    Algoritmo final

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
-#Mergesort recursivo que ordene de mayor a menor
-def DACmergesort(n, array):
-    if (n>1):
-        mid = n//2
-        left_array = array[0:mid]
-        right_array = array[mid+1:n-1]
-        
-    DACmergesort(len(left_array),left_array)
-    DACmergesort(len(left_array),left_array)
-        
+# Juan Carlos Ferrer Echeverría A01734794
+# Alejandro Alfonso Ubeto Yañez A01734977
+# Maximiliano Romero Budib      A01732008
 
-        
-    
+#Mergesort recursivo que ordene de mayor a menor
+def merge(left, right):
+    i, j = 0, 0
+    sorted = []
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            sorted.append(left[i])
+            i += 1
+        else: 
+            sorted.append(right[j])
+            j += 1
+
+    sorted += left[i:]
+    sorted += right[j:]
+    return sorted
+
+#Función que ordena array de elementos aplicando función recursiva
+def DACmergesort(n, array):
+
+    #Caso base: no es posible dividir más el array
+    if (n<=1):
+        return array
+
+    else:
+        mid = n//2
+        left_array = DACmergesort(len(array[:mid]),array[:mid])
+        right_array = DACmergesort(len(array[mid:]),array[mid:])
+        return merge(left_array, right_array)
+
+lista =  [2,7,1]
+print(DACmergesort(len(lista),lista))
+
