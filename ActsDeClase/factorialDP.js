6b69cf46b824ee1b90a6db303bd8321d073f4d1c
ale-local <alejo.ubeto@gmail.com>
2022-08-15 08:32:15 -0500

    Enfoque iterativo de factorial con arreglos

--- ActsDeClase/factorialDP.js
+++ ActsDeClase/factorialDP.js
@@ -7,8 +7,6 @@ function factorialDP(n){
     return fac;
 }
 
-
-
 //Versión recursiva
 function factorialRecursivo(n){
     if(n<=1)
@@ -17,6 +15,19 @@ function factorialRecursivo(n){
         return 2;
     return n*factorialRecursivo(n-1);
 }
+
+//Versión iterativa con arreglos de Óscar y Rocha
+var f = [];
+f[0] = 1;
+
+const factorial_arreglos = (n) => {
+    for(let i=1; i<=n; i++){
+        f[i] = i * f[i-1];
+    }
+    return f[n];
+}
+
 //Ejecuciones
 console.log(factorialDP(10));
-console.log(factorialRecursivo(10));
+console.log(factorialRecursivo(01));
+console.log(factorial_arreglos(10));
