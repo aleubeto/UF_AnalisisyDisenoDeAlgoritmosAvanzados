1b1b83eacda1e3fbe89149a83ee79230d9b4f3f0
JuanFerrxr <jcferrere1032@gmail.com>
2022-08-20 15:00:19 -0500

    Primer commit de act1.2

--- Tareas/Act1.2/main.py
+++ Tareas/Act1.2/main.py
@@ -2,3 +2,23 @@
 # Alejandro Alfonso Ubeto Yañez A01734977
 # Maximiliano Romero Budib      A01732008
 
+#Implementación de PD y algoritmos avaros para el cambio de monedas
+
+def dinamic(n,p,q):
+    
+    return 0
+
+
+
+
+#Introducción de los valores
+
+print("Introduce el número de denominaciones de monedas")
+n = input()
+
+print("Introduce el precio del producto:")
+p = input()
+
+print("Introduce el billete o moneda(s) de pago:")
+q = input()
+
