a4362994fed40e3c014fd6ddb501fd50f1a887f9
JuanFerrxr <jcferrere1032@gmail.com>
2022-08-20 19:03:56 -0500

    Función de lista de cambio vacía implementada

--- Tareas/Act1.2/main.py
+++ Tareas/Act1.2/main.py
@@ -4,30 +4,40 @@
 
 #Implementación de PD y algoritmos avaros para el cambio de monedas
 
+def change_list(n):
+    change = []
+    for i in range(n):
+        change.append(0)
+    return(change)
+
 def dinamic(n,coins,p,q):
+    change = change_list(n)
     
-    return 0
+    return change
 
 def greedy(n,coins,p,q):
-    
-    return 0
+    change = change_list(n)
+    i = n - 1
+    while (i>=0):
+        return 0
+    return change
 
 
 
 #Introducción de los valores vía teclado
-n = int(input("Introduce el número de denominaciones de monedas: "))
+n = int(input("+ Introduce el número de denominaciones de monedas: "))
 coins = []
 for i in range(n):
-    coins.append(int(input("Introduzca el valor " + str(i + 1) + " de denomicación de las monedas: ")))
-p = int(input("Introduce el precio del producto: "))
-q = int(input("Introduce el billete o moneda(s) de pago: "))
+    coins.append(int(input("- Introduzca el valor " + str(i + 1) + " de denomicación de las monedas: ")))
+p = int(input("+ Introduce el precio del producto: "))
+q = int(input("+ Introduce el billete o moneda(s) de pago: "))
 
 # Ejecución de funciones
 dinamic(n,coins,p,q)
 greedy(n,coins,p,q)
 
 # Ejecuciones de prueba
-dinamic(4,[1,2,5,10], 20, 30)
-greedy(4,[1,2,5,10], 20, 30)
+#dinamic(4,[1,2,5,10], 20, 30)
+#greedy(4,[1,2,5,10], 20, 30)
 
 
