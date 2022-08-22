37d9849365fe100570a5e90b00e407c326acbed9
ale ubeto <alejo.ubeto@gmail.com>
2022-08-21 22:30:26 -0500

    Implementación de función main() para ejecutar el programa

--- Tareas/Act1.2/main.py
+++ Tareas/Act1.2/main.py
@@ -6,7 +6,8 @@
 
 #Función de cambio con progra dinámica
 def dinamic(n,coins,p,q):
-    change = change_list(n)
+    #change = change_list(n)
+    change = 0
     
     return change
 
@@ -16,31 +17,25 @@ def greedy(n,coins,p,q):
     i = n - 1
     while (i>=0):
         if (change >= coins[i]):
-            print(change//coins[i])
+            print(str(coins[i]) + ':', change//coins[i])
             change = change % coins[i]
         else:
-            print(0)
+            print(str(coins[i]) + ':', 0)
         i -= 1
         
-        
-
-
-
-
 #Introducción de los valores vía teclado
-n = int(input("+ Introduce el número de denominaciones de monedas: "))
-coins = []
-for i in range(n):
-    coins.append(int(input("- Introduzca el valor " + str(i + 1) + " de denomicación de las monedas: ")))
-p = int(input("+ Introduce el precio del producto: "))
-q = int(input("+ Introduce el billete o moneda(s) de pago: "))
+def main():
+    n = int(input("+ Introduce el número de denominaciones de monedas: "))
+    coins = []
+    for i in range(n):
+        coins.append(int(input("- Introduzca el valor " + str(i + 1) + " de denomicación de las monedas: ")))
+    p = int(input("+ Introduce el precio del producto: "))
+    q = int(input("+ Introduce el billete o moneda(s) de pago: "))
 
-# Ejecución de funciones
-dinamic(n,coins,p,q)
-greedy(n,coins,p,q)
+    # Ejecución de funciones
+    dinamic(n,coins,p,q)
+    greedy(n,coins,p,q)
 
 # Ejecuciones de prueba
 dinamic(4,[1,2,5,10], 20, 30)
 greedy(4,[1,2,5,10], 20, 35)
-
-
