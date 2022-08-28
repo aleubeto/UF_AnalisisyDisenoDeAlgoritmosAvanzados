863d5a82bdda63c66344c603b9a54df72e603f88
ale ubeto <alejo.ubeto@gmail.com>
2022-08-28 17:37:25 -0500

    Funciones de ejecución y prueba (print_map y main)

--- Tareas/Act1.3/main.py
+++ Tareas/Act1.3/main.py
@@ -1,7 +1,37 @@
 #Método 1: Backtracking
-def backtracking_solution():
-    return 0
+def backtracking_solution(map):
+    return map
 
 #Método 2: Ramificación y poda (branch and bound)
-def bnb_solution():
-    return 0
+def bnb_solution(map):
+    return map
+
+#Función para imprimir mapa
+def print_map(map):
+    print('')
+    for i in range(len(map)):
+        line = ''
+        for j in range(len(map[i])):
+            line += f'{map[i][j]} '
+        print(line)
+
+#Ejecución
+def main():
+    m = int(input('m: '))
+    n = int(input('n: '))
+    map = []
+    for i in range(m):
+        line = input()
+        map.append(line.split()[:n])
+    #Solución con Backtracking
+    backtracking = backtracking_solution(map)
+    print_map(backtracking)
+    #Solución con Branch and Bound
+    bnb = bnb_solution(map)
+    print_map(bnb)
+
+#Casos de prueba
+#test1 = [[1,0,0,0], [1,1,0,1], [0,1,0,0], [1,1,1,1]]
+#print_map(backtracking_solution(test1))
+#print_map(bnb_solution(test1))
+main()
