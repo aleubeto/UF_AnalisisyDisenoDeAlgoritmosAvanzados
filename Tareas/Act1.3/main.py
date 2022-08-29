75d2b1019786387fa575d5d274c12b1f90ffb819
MaxRBudib <maximilianoromerobudib@yahoo.com.mx>
2022-08-28 23:32:51 -0500

    Solucion backtracking agregada

--- Tareas/Act1.3/main.py
+++ Tareas/Act1.3/main.py
@@ -1,6 +1,39 @@
 #Método 1: Backtracking
-def backtracking_solution(map):
-    return map
+def isValid(a, h, map, x, y, res):
+    if x >= 0 and y >= 0 and x < h and y < a and map[x][y] == 1 and res[x][y] == 0:
+        return True
+    return False
+ 
+ 
+ 
+def RatMaze(a, h, map, move_x, move_y, x, y, res):
+    if x == h-1 and y == a-1:
+        return True
+    for i in range(4):
+        x_new = x + move_x[i]
+ 
+        y_new = y + move_y[i]
+ 
+        if isValid(a, h, map, x_new, y_new, res):
+ 
+            res[x_new][y_new] = 1
+            if RatMaze(a, h, map, move_x, move_y, x_new, y_new, res):
+                return True
+            res[x_new][y_new] = 0
+    return False
+
+def backtracking_solution(a, h, map):
+    res = [[0 for i in range(a)] for i in range(h)]
+    res[0][0] = 1
+
+    move_x = [-1, 1, 0, 0]
+
+    move_y = [0, 0, -1, 1]
+ 
+    if RatMaze(a, h, map, move_x, move_y, 0, 0, res):
+        return(res)
+    else:
+        return("Solution does not exist")
 
 #Método 2: Ramificación y poda (branch and bound)
 def bnb_solution(map):
@@ -22,9 +55,9 @@ def main():
     map = []
     for i in range(m):
         line = input()
-        map.append(line.split()[:n])
+        map.append([int(x) for x in line.split()[:n]])
     #Solución con Backtracking
-    backtracking = backtracking_solution(map)
+    backtracking = backtracking_solution(n, m, map)
     print_map(backtracking)
     #Solución con Branch and Bound
     bnb = bnb_solution(map)
