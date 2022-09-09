602722a37beae34f46e7917f3d3886b34f6fe802
ale ubeto <alejo.ubeto@gmail.com>
2022-09-08 23:09:55 -0500

    Funciones de actividad Listas

--- Actividad integradora/ActInt1/main.py
+++ Actividad integradora/ActInt1/main.py
@@ -19,7 +19,7 @@ def KMPSearch(pat, txt):
             j += 1
  
         if j == M:
-            print ("Found pattern at index", str(i-j))
+            return(i-j)
             match = True
             j = lps[j-1]
  
@@ -32,7 +32,7 @@ def KMPSearch(pat, txt):
             else:
                 i += 1
     if not match:
-        print("No match")
+        return ''
  
 def computeLPSArray(pat, M, lps):
     len = 0 # length of the previous longest prefix suffix
@@ -57,56 +57,73 @@ def computeLPSArray(pat, M, lps):
             else:
                 lps[i] = 0
                 i += 1
- 
+         
+         
+#Función de la parte 2 para buscar mcode espejeado
+def output2(transmission, mcodeList):
+    espejeado = [[],[]]
+    for i in mcodeList:
+        if KMPSearch(i[::-1],transmission) != '':
+            espejeado[0].append(KMPSearch(i[::-1],transmission)+1)
+            espejeado[1].append(KMPSearch(i[::-1],transmission)+len(i)+1)
+    if len(espejeado[0])>0:
+        first_found = espejeado[0].index(min(espejeado[0]))
+        return f'{espejeado[0][first_found]} {espejeado[1][first_found]}'
+    else:
+        return 'No match'       
+
+#Función de la parte 3 que genera los substrings de un archivo 
+def substrings(txt):
+    txt = txt[::-1]
+    s = []
+    for i in range(len(txt)):
+        if s == []:
+            s.append(txt[i])
+        else:
+            s.append(txt[i] + s[i-1])
+    return(s)
 
 
-transOne = open("transmission1.txt", "r")
-transTwo = open("transmission2.txt", "r")
-mcodeOne = open("mcode1.txt", "r")
-mcodeTwo = open("mcode2.txt", "r")
-mcodeThree = open("mcode3.txt")
+#Función de parte 3 que busca el substring mayor de un archivo (transmission1) en otro (transmission2)
+def compare(txt1, txt2):
+    pat_list = substrings(txt1)
+    beginning = 0
+    ending = 0
+    max = ''
+    for i in range(len(pat_list)):
+        for j in range(len(txt2)):
+            if pat_list[i] == txt2[j:j+len(pat_list[i])]:
+                if len(pat_list[i]) > len(max):
+                    beginning = j + 1
+                    ending = j+len(pat_list[i])
+                    max = pat_list[i]
 
-transOne = transOne.read()
-transTwo = transTwo.read()
-mcodeOne = mcodeOne.read()
-mcodeTwo = mcodeTwo.read()
-mcodeThree = mcodeThree.read()
+    print(f"Inicial:{beginning}       Final:{ending}     Substring:/{max}/")
 
-transOne = transOne.upper()
-transTwo = transTwo.upper()
-mcodeOne = mcodeOne.upper()
-mcodeTwo = mcodeTwo.upper()
-mcodeThree = mcodeThree.upper()
+transOne = open("transmission1.txt").read().upper()
+transTwo = open("transmission2.txt").read().upper()
+mcodeOne = open("mcode1.txt").read().upper()
+mcodeTwo = open("mcode2.txt").read().upper()
+mcodeThree = open("mcode3.txt").read().upper()
 
-print(transOne)
-print(mcodeOne)
-#mcodeOne es el patron a encontrar, transOne es el string donde esta el patron
-KMPSearch(mcodeOne, transOne)
 
-print(transOne)
-print(mcodeTwo)
+#transmission1.txt Parte 1
+print("\nParte 1")
+print(f"{True if KMPSearch(mcodeOne, transOne) != '' else False} {KMPSearch(mcodeOne, transOne)}")
+print(f"{True if KMPSearch(mcodeTwo, transOne) != '' else False} {KMPSearch(mcodeTwo, transOne)}")
+print(f"{True if KMPSearch(mcodeThree, transOne) != '' else False} {KMPSearch(mcodeThree, transOne)}")
 
-KMPSearch(mcodeTwo, transOne)
+#transmission2.txt Parte 1
+print(f"{True if KMPSearch(mcodeOne, transTwo) != '' else False} {KMPSearch(mcodeOne, transTwo)}")
+print(f"{True if KMPSearch(mcodeTwo, transTwo) != '' else False} {KMPSearch(mcodeTwo, transTwo)}")
+print(f"{True if KMPSearch(mcodeThree, transTwo) != '' else False} {KMPSearch(mcodeThree, transTwo)}")
 
-print(transOne)
-print(mcodeThree)
+#Parte 2
+print("\nParte 2")
+mcodeList = [mcodeOne, mcodeTwo, mcodeThree]
+print(output2(transOne,mcodeList))
+print(output2(transTwo,mcodeList))
 
-KMPSearch(mcodeThree, transOne)
-
-#Archivo 2
-print(transTwo)
-print(mcodeOne)
-
-KMPSearch(mcodeOne, transTwo)
-
-print(transTwo)
-print(mcodeTwo)
-
-KMPSearch(mcodeTwo, transTwo)
-
-print(transTwo)
-print(mcodeThree)
-
-KMPSearch(mcodeThree, transTwo)
- 
-# This code is contributed by Bhavya Jain
+#Parte 3
+print("\nParte 3")
+compare(transOne, transTwo)
