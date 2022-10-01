c58518f5fda67717bde960bb1c9e607b12ac4fe4
ale ubeto <alejo.ubeto@gmail.com>
2022-10-01 17:17:59 -0500

    Función principal + Complejidad algorítmica

--- Tareas/Act3.3/main.py
+++ Tareas/Act3.3/main.py
@@ -7,9 +7,8 @@
 # weights =  enteros que representan los pesos asociados a cada elemento
 # w = la capacidad o peso máximo de la mochila
 
-def knapsack(n, values, weights, w):
+def knapsack(n, values, weights, w): #Orden cuadrático O(n**2)
     K = [[0 for x in range(w+1)] for x in range(n+1)]
-    # Build table K[][] in bottom up manner
     for i in range(n + 1):
         for w in range(w + 1):
             if i == 0 or w == 0:
@@ -20,6 +19,19 @@ def knapsack(n, values, weights, w):
                 K[i][w] = K[i-1][w]
     return K[n][w]
 
+def main(): #Orden lineal O(n)
+    n = int(input())
+    vals = []
+    wgth = []
+    for i in range(n):
+        vals.append(int(input()))
+    for i in range(n):
+        wgth.append(int(input()))
+    w = int(input())
+    print(f'\n{knapsack(n,vals,wgth,w)}')
 
+# Ejecución de programa principal
+main()
+
+# Casos de prueba
 print(knapsack(3, [1,2,3], [4,5,1], 4))
-
