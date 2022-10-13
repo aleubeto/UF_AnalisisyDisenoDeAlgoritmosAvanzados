48975e9d39df1a9e203cf9628a9cdc83612ae8af
JuanFerrxr <jcferrere1032@gmail.com>
2022-10-12 19:15:26 -0500

    pruebas implementadas

--- Tareas/Act3.4/main.js
+++ Tareas/Act3.4/main.js
@@ -1,38 +1,67 @@
 //    tsc filename.ts
 //    node filename.js
-var tamano = 5;
-var graph = [
+function coloring(tamano, graph) {
+    var mapa = new Map();
+    for (var i = 0; i < tamano; i++) {
+        mapa.set(graph[i], -1);
+    }
+    for (var i = 0; i < tamano; i++) {
+        for (var j = 0; j < tamano; j++) {
+            if (graph[i][j] == 1) {
+                if (mapa.get(graph[j]) == mapa.get(graph[i])) {
+                    mapa.set(graph[i], (Math.max(mapa.get(graph[i]), mapa.get(graph[j])) + 1));
+                    j = 0;
+                }
+            }
+        }
+    }
+    for (var i = 0; i < tamano; i++) {
+        mapa.set(graph[i], 0);
+        for (var j = 0; j < tamano; j++) {
+            if (graph[i][j] == 1) {
+                if (mapa.get(graph[j]) == mapa.get(graph[i])) {
+                    mapa.set(graph[i], (Math.max(mapa.get(graph[i]), mapa.get(graph[j])) + 1));
+                    j = 0;
+                }
+            }
+        }
+    }
+    for (var i = 0; i < tamano; i++) {
+        console.log("Node: " + (i + 1) + " , Assigned Color: " + mapa.get(graph[i]));
+    }
+    console.log("\n");
+}
+var tamano1 = 5;
+var graph1 = [
     [0, 0, 1, 0, 1],
     [0, 0, 1, 1, 1],
     [1, 1, 0, 1, 0],
     [0, 1, 1, 0, 1],
     [1, 1, 0, 1, 0], //5
 ];
-var mapa = new Map();
-for (var i = 0; i < tamano; i++) {
-    mapa.set(graph[i], -1);
-}
-for (var i = 0; i < tamano; i++) {
-    for (var j = 0; j < tamano; j++) {
-        if (graph[i][j] == 1) {
-            if (mapa.get(graph[j]) == mapa.get(graph[i])) {
-                mapa.set(graph[i], (Math.max(mapa.get(graph[i]), mapa.get(graph[j])) + 1));
-                j = 0;
-            }
-        }
-    }
-}
-for (var i = 0; i < tamano; i++) {
-    mapa.set(graph[i], 0);
-    for (var j = 0; j < tamano; j++) {
-        if (graph[i][j] == 1) {
-            if (mapa.get(graph[j]) == mapa.get(graph[i])) {
-                mapa.set(graph[i], (Math.max(mapa.get(graph[i]), mapa.get(graph[j])) + 1));
-                j = 0;
-            }
-        }
-    }
-}
-for (var i = 0; i < tamano; i++) {
-    console.log("Node: " + (i + 1) + " , Assigned Color: " + mapa.get(graph[i]));
-}
+var tamano2 = 5;
+var graph2 = [
+    [0, 1, 1, 1, 0],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 1, 1],
+    [1, 0, 1, 0, 1],
+    [0, 1, 1, 1, 0], //5
+];
+var tamano3 = 4;
+var graph3 = [
+    [0, 1, 1, 0],
+    [1, 0, 1, 1],
+    [1, 1, 0, 0],
+    [0, 1, 0, 0], //4
+];
+var tamano4 = 4;
+var graph4 = [
+    [0, 1, 0, 1],
+    [1, 0, 1, 0],
+    [0, 1, 0, 1],
+    [1, 0, 1, 0], //4
+];
+coloring(tamano1, graph1);
+coloring(tamano2, graph2);
+coloring(tamano3, graph3);
+coloring(tamano4, graph4);
--- Tareas/Act3.4/main.ts
+++ Tareas/Act3.4/main.ts
@@ -2,9 +2,44 @@
 //    tsc filename.ts
 //    node filename.js
 
+function coloring(tamano, graph) {
+    let mapa = new Map();
 
-let tamano  = 5;
-let graph = [
+    for (let i = 0; i < tamano; i++){
+        mapa.set(graph[i], -1)
+    }
+
+    for (let i = 0; i < tamano; i++){
+        for (let j = 0; j < tamano; j++){
+            if (graph[i][j] == 1){
+                if (mapa.get(graph[j]) == mapa.get(graph[i])){
+                    mapa.set(graph[i], (Math.max(mapa.get(graph[i]), mapa.get(graph[j])) + 1))
+                    j = 0
+                } 
+            }
+        }
+    }
+
+    for (let i = 0; i < tamano; i++){
+        mapa.set(graph[i], 0)
+        for (let j = 0; j < tamano; j++){
+            if (graph[i][j] == 1){
+                if (mapa.get(graph[j]) == mapa.get(graph[i])){
+                    mapa.set(graph[i], (Math.max(mapa.get(graph[i]), mapa.get(graph[j])) + 1))
+                    j = 0
+                } 
+            }
+        }
+    }
+
+    for (let i = 0; i < tamano; i++){
+        console.log("Node: " + (i+1) + " , Assigned Color: " + mapa.get(graph[i]))
+    }
+    console.log("\n")
+}
+
+let tamano1  = 5;
+let graph1 = [
     [0,0,1,0,1], //1
     [0,0,1,1,1], //2s
     [1,1,0,1,0], //3
@@ -12,36 +47,32 @@ let graph = [
     [1,1,0,1,0], //5
 ];
 
-let mapa = new Map();
+let tamano2  = 5;
+let graph2 = [
+    [0,1,1,1,0], //1
+    [1,0,0,0,1], //2s
+    [1,0,0,1,1], //3
+    [1,0,1,0,1], //4
+    [0,1,1,1,0], //5
+];
 
-for (let i = 0; i < tamano; i++){
-    mapa.set(graph[i], -1)
-}
+let tamano3  = 4;
+let graph3 = [
+    [0,1,1,0], //1
+    [1,0,1,1], //2s
+    [1,1,0,0], //3
+    [0,1,0,0], //4
+];
 
-for (let i = 0; i < tamano; i++){
-    for (let j = 0; j < tamano; j++){
-        if (graph[i][j] == 1){
-            if (mapa.get(graph[j]) == mapa.get(graph[i])){
-                mapa.set(graph[i], (Math.max(mapa.get(graph[i]), mapa.get(graph[j])) + 1))
-                j = 0
-            } 
-        }
-    }
-}
+let tamano4  = 4;
+let graph4 = [
+    [0,1,0,1], //1
+    [1,0,1,0], //2s
+    [0,1,0,1], //3
+    [1,0,1,0], //4
+];
 
-for (let i = 0; i < tamano; i++){
-    mapa.set(graph[i], 0)
-    for (let j = 0; j < tamano; j++){
-        if (graph[i][j] == 1){
-            if (mapa.get(graph[j]) == mapa.get(graph[i])){
-                mapa.set(graph[i], (Math.max(mapa.get(graph[i]), mapa.get(graph[j])) + 1))
-                j = 0
-            } 
-        }
-    }
-}
-
-
-for (let i = 0; i < tamano; i++){
-    console.log("Node: " + (i+1) + " , Assigned Color: " + mapa.get(graph[i]))
-}
+coloring(tamano1, graph1)
+coloring(tamano2, graph2)
+coloring(tamano3, graph3)
+coloring(tamano4, graph4)
