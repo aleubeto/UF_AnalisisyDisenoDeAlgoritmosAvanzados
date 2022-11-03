1bb0d0585939007cded53d5d3f723c423b0dd2f8
JuanFerrxr <jcferrere1032@gmail.com>
2022-11-03 08:12:33 -0600

    Trabajo inicial

--- Tareas/Act4.3/main.py
+++ Tareas/Act4.3/main.py
@@ -2,10 +2,27 @@
 # Alejandro Alfonso Ubeto Yañez A01734977
 # Maximiliano Romero Budib      A01732008
 
+class Dot:
+    def __init__(self, x = None, y = None):
+        self.x = x
+        self.y = y
+
+
+
 def main():
     n = int(input())
+    point=()
     points = []
+    dots=[]
     for i in range(n):
-        print(i)
+        point = input().split(",")
+        points.append(point)
+    print(points)
+    
+    for i in points:
+        dots.append(Dot(i[0], i[1]))
+    
+
+    
 
 main()
