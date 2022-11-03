c384bd8c921ecb718200f57b9cec6e16889684be
JuanFerrxr <jcferrere1032@gmail.com>
2022-11-03 07:49:32 -0600

    Creación de branch 4.3

---
+++ Tareas/Act4.3/main.py
@@ -0,0 +1,11 @@
+# Juan Carlos Ferrer Echeverría A01734794
+# Alejandro Alfonso Ubeto Yañez A01734977
+# Maximiliano Romero Budib      A01732008
+
+def main():
+    n = int(input())
+    points = []
+    for i in range(n):
+        print(i)
+
+main()
