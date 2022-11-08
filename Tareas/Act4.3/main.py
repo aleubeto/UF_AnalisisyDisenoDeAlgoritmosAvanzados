c1715d02d62c89c5a70bd4973251686d83c1b692
JuanFerrxr <jcferrere1032@gmail.com>
2022-11-07 19:28:53 -0600

    Función implementada

--- Tareas/Act4.3/main.py
+++ Tareas/Act4.3/main.py
@@ -7,22 +7,54 @@ class Dot:
         self.x = x
         self.y = y
 
+#Función de distancia entre dos puntos
+def distancia(p1, p2):
+    	return (float((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y)))
 
 
+def bg(n, dots):
+    d_min1 = 0
+    d_min2 = float('inf')
+    for i in range(n):  #for j = i+1, j<n
+        for y in range(i+1,n):   
+            d_min1 = distancia(dots[i], dots[y])
+            d_min2 =  min(d_min1, d_min2)
+    
+    print(d_min2)
+            
+    #print(f"La distancia más corta es: {d_min2}")
+    
+    
 def main():
     n = int(input())
     point=()
     points = []
     dots=[]
     for i in range(n):
-        point = input().split(",")
+        point = (input().split(","))
+        point[0]=int(point[0])
+        point[1]=int(point[1])
+        point=list(point)
         points.append(point)
     print(points)
     
     for i in points:
         dots.append(Dot(i[0], i[1]))
+        
+    print(dots)
     
 
     
+def testcase(n, dots):
+    array = []
+    for i in dots:
+        array.append(Dot(i[0],i[1]))
+    #print(array)
+    return(bg(n, array))
+#main()
 
-main()
+tc1 = [(2, 3), (12, 30), (40, 50), (5, 1), (12, 10), (3, 4)]
+#testcase(len(tc1), tc1)
+
+tc2 = [(1, 1), (22, 1), (4, 1), (1, 1), (2, 1)]
+testcase(len(tc2), tc2)
