06943a810a47c3e3dff26a17e13053b54e966e44
ale_wsl <alejo.ubeto@gmail.com>
2022-11-07 23:00:36 -0600

    Testcases añadidos

--- Tareas/Act4.4/main.py
+++ Tareas/Act4.4/main.py
@@ -2,11 +2,10 @@
 # Alejandro Alfonso Ubeto Yañez A01734977
 # Maximiliano Romero Budib      A01732008
 
-# Función que realiza una busqueda binaria aleatoria
-
 import math
 import random
 
+# Función que realiza una busqueda binaria aleatoria
 def randomBinarySearch(search, arr):
     start = 0
     end = len(arr) - 1
@@ -17,10 +16,8 @@ def randomBinarySearch(search, arr):
         elif search > arr[mid]:
             start = mid + 1
         else:
-            print("El elemento esta presente en el indice: ", mid)
-            return 0 
-    print("El elemento no esta presente en el arreglo")
-    return 0
+            return f"El elemento esta presente en el indice: {mid}"
+    return "El elemento no esta presente en el arreglo"
 
 # Función que ejecuta programa principal
 def main():
@@ -30,8 +27,17 @@ def main():
     dataset = input().split(',')
     for i in range(len(dataset)):
         dataset[i] = int(dataset[i])
-    randomBinarySearch(search, dataset)
-    return 0
+    print(randomBinarySearch(search, dataset))
+
+# Función de casos de prueba
+def testcase(s,d):
+    print(randomBinarySearch(s,d))
 
 # Ejecución de programa principal
 main()
+
+# Ejecución de casos de prueba
+testcase(10,[2,3,4,10,40])  #1
+testcase(11,[2,3,4,10,40])  #2
+testcase(6,[1,3,6,7,2,20])  #3
+testcase(3,[1,3,5,20,100])  #4
