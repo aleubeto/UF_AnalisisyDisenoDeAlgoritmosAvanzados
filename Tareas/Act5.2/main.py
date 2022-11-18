4f6a830b44f9aada161d4b558a3b0b9a2b556699
ale-argos <alejo.ubeto@gmail.com>
2022-11-18 17:44:07 -0600

    Seguimos trabajando

--- Tareas/Act5.2/main.py
+++ Tareas/Act5.2/main.py
@@ -2,36 +2,36 @@
 # Alejandro Alfonso Ubeto Yañez A01734977
 # Maximiliano Romero Budib      A01732008
 
-# Función que realiza algortmo de Backtracking con Bitmask
-def backtracking_bitmask(matrix):
-    permutation = []
-    result = []
-    for i in range(len(matrix[0])):    
-        permutation.append(matrix[0][i])
-        for j in range(1,len(matrix)):
-            for k in range(len(matrix[j])):
-                if matrix[j][k] not in permutation:
-                    permutation.append(matrix[j][k])
-                    if len(permutation) == len(matrix)-1:
-                        result.append(permutation)
-        permutation=[]
-    
-    print(result)
-                    
-        
-        
-        
-    """
+# Función que genera matriz para realizar programación dinámica
+def dinamic_chart(matrix):
     dinamicChart = []
     for i in range(len(matrix)):
         temp = []
         for j in range(100):
             if j+1 in matrix[i]:
-                temp.append(True)
+                temp.append(1)
             else:
-                temp.append(False)
+                temp.append(0)
         dinamicChart.append(temp)
-    """
+    return dinamicChart
+
+# Función que realiza algortmo de Backtracking con Bitmask
+def backtracking_bitmask(matrix,pivote,i,j,temp,result):
+    return 0
+
+
+'''def backtracking_bitmask(matrix,i,j,result):
+    # Caso base
+    if i==len(matrix):
+        return result
+    else:
+        result.append(matrix[i][:j+1])
+        if j==len(matrix[i])-1:
+            i+=1
+            j=0
+        else:
+            j+=1
+        return backtracking_bitmask(matrix,i,j,result)'''
 
 # Función de programa principal
 def main():
@@ -56,11 +56,13 @@ def main():
 
 # Función de casos de prueba
 def tescase(tc):
-    backtracking_bitmask(tc)
+    #dc = dinamic_chart(tc)
+    #print(backtracking_bitmask(tc,0,0,[]))
+    print(backtracking_bitmask(tc,0,0,[],[]))
 
 # Ejecución de función de programa principal
-main()
+#main()
 
 # Ejecución de casos de prueba
 tc1 = [[5,100,1],[2],[5,100]]
-#tescase(tc1)
+tescase(tc1)
