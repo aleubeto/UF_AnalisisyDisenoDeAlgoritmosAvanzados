167476096f00a7fb977c1fb8f14ff6e7f246c7d6
JuanFerrxr <jcferrere1032@gmail.com>
2022-12-03 00:07:58 -0600

    Tarea terminada

--- Tareas/Act5.7/main.py
+++ Tareas/Act5.7/main.py
@@ -2,30 +2,60 @@
 # Alejandro Alfonso Ubeto Yañez A01734977
 # Maximiliano Romero Budib      A01732008
 
-# Función que implementa algoritmo Hill-climbing
-def hillclimbing(n):
-    # A TRABAJAR!!!
-    return []
+# Verificamos que la reina n oesté siendo atacada
+def attacked(i, j,matrix,N):
+    for k in range(0,N):
+        if matrix[i][k]==1 or matrix[k][j]==1:
+            return True
+    for k in range(0,N):
+        for l in range(0,N):
+            if (k+l==i+j) or (k-l==i-j):
+                if matrix[k][l]==1:
+                    return True
+    return False
 
-# Función que imprime el resultado
-def printresult(result):
-    for i in result:
-        print(i)
+#Función principal de Hill Climbing
+def hillclimbing(n,matrix,N):
+    if n==0:
+        return True
+    for i in range(0,N):
+        for j in range(0,N):
 
-# Función que ejecuta programa principal
+            if (not(attacked(i,j,matrix,N))) and (matrix[i][j]!=1):
+                matrix[i][j] = 1
+                if hillclimbing(n-1,matrix,N)==True:
+                    return True
+                matrix[i][j] = 0
+
+    return False
+
+#Función main de ejecución manual
 def main():
-    n = int(input())
-    solution = hillclimbing(n)
-    printresult(solution)
-
-# Función que ejecuta un caso de prueba
-def tc(testcase):
-    solution = hillclimbing(testcase)
-    printresult(solution)
-
-# Ejecución de programa principal
+    N = int(input())
+    matrix = [[0]*N for _ in range(N)]
+    hillclimbing(N,matrix,N)
+    for i in matrix:
+        print (i)
+        
 main()
 
-# Ejecución de casos de prueba
-tc(4)   #tc1
-tc(8)   #tc2
+#Funcion testcases
+def testcase(N):
+    matrix = [[0]*N for _ in range(N)]
+    hillclimbing(N,matrix,N)
+    for i in matrix:
+        print (i)
+
+print("Caso de prueba n = 4")        
+testcase(4)
+
+print("Caso de prueba n = 8")        
+testcase(8)
+
+print("Caso de prueba n = 12")        
+testcase(12)
+
+print("Caso de prueba n = 0")        
+testcase(0)
+
+        
